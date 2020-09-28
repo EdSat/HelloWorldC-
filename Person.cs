3adefcb221baa74d9b9d3ea26c4f3149c9a7654b
EdSat <editheding@hotmail.de>
2020-09-29 00:48:28 +0200

    Klasse Person erstellt +Ausgabe und Eingabe in Console

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Person(string name1, int age1)
+        {
+            name = name1;
+            age = age1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Hans", 20);
+            Console.WriteLine("Hello {0}{1} {2}", person1.name, person1.age, "The End!");
+            
+            Person person2 = person1;
+            person2.name = "Holly";
+            person2.age = 18;
+            Console.WriteLine("And hello to " + person2.name + person2.age + "Type a cool name");
+
+            String text = Console.ReadLine();
+            Console.WriteLine(text + " is the best!");
+        }
+    }
+}
